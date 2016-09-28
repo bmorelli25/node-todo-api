b2e546853463414effc9aebfa66e92cd0941e2fa
pentel13 <bmorelli25@gmail.com>
2016-09-28 15:35:02 -0700

    add GET /todos/:id

--- server.js
+++ server.js
@@ -24,8 +24,23 @@ app.get('/todos', function (req, res) {
   res.json(todos);
 });
 
-// GET /todos/:id
+// GET /todos/:id - return individual todo item
+app.get('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id);
+  var matchedTodo;
 
+  todos.forEach(function (todo) {
+    if (todo.id === todoId){
+      matchedTodo = todo;
+    };
+  });
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();
+  };
+});
 
 app.listen(PORT, function () {
   console.log('Express listening on PORT ' + PORT);
