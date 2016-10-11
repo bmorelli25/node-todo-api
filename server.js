4a38c1df5ee579234bc16a492349c2b757cb14bf
pentel13 <bmorelli25@gmail.com>
2016-10-11 15:00:36 -0700

    Add todo model and POST /todos
    sqlite

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -51,16 +52,11 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
-  // validate data provided
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send(); // 400 means bad data provided
-  };
-
-  body.description = body.description.trim();
-  body.id = todoNextId++;
-
-  todos.push(body);
-  res.json(body); // pass body back to the user
+  db.todo.create(body).then((todo) => {
+    res.json(todo.toJSON());
+  }, (e) => {
+    res.status(400).json(e);
+  });
 });
 
 // DELETE /todos/:id
@@ -103,6 +99,8 @@ app.put('/todos/:id', function (req, res) {
   res.json(matchedTodo);
 });
 
-app.listen(PORT, function () {
-  console.log('Express listening on PORT ' + PORT);
+db.sequelize.sync().then(() => {
+  app.listen(PORT, function () {
+    console.log('Express listening on PORT ' + PORT);
+  });
 });
