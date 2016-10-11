a444a880ec4de2a186338a6c366148e74acce714
pentel13 <bmorelli25@gmail.com>
2016-10-11 12:44:33 -0700

    Playing with Sequelize

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -6,21 +6,58 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
   description: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 250]
+    }
   },
   completed: {
-    type: Sequelize.BOOLEAN
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
   }
 });
 
-sequelize.sync({force: true}).then( () => {
+sequelize.sync({
+  //force: true
+}).then(() => {
   console.log('Everything is synced');
 
-  Todo.create({
-    description: 'Walk the dog',
-    completed: false
-  }).then((todo) => {
-    console.log('Finished!');
-    console.log(todo);
-  });
+  return Todo.findById(2);
+
+  // Todo.create({
+  //   description: 'Walk the dog'
+  // }).then((todo) => {
+  //   return Todo.create({
+  //     description: 'Clean office'
+  //   });
+  // }).then(() => {
+  //   //return Todo.findById(1)
+  //   return Todo.findAll({
+  //     where: {
+  //       description: {
+  //         $like: '%Office%'
+  //       }
+  //     }
+  //   });
+  // }).then((todos) => {
+  //   if (todos) {
+  //     todos.forEach((todo) => {
+  //       console.log(todo.toJSON());
+  //     });
+  //   } else {
+  //     console.log('NO TODO FOUND');
+  //   };
+  // }).catch((e) => {
+  //   console.log(e);
+  // });
+}).then((todo) => {
+  if (todo) {
+    console.log(todo.toJSON());
+  } else {
+    console.log('NO TODO FOUND');
+  };
+}).catch((e) => {
+  console.log(e);
 });
